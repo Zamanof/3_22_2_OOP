546a73bc5810e9ecbb33cc733d162a53d5ba265d
Zamanof <Zamanov@itstep.edu.az>
2023-06-21 17:38:17 +0400

    pure virtual

---
+++ Pure_virtual_methods/Pure_virtual.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+class Animal {
+public:
+	string name;
+	// Pure virtual method
+	virtual void Sound() = 0;
+};
+
+class Dog : public Animal {
+public:
+	int age;
+	void Sound() override {
+		cout << name << " Gaw gaw" << endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	float price;
+	virtual void Sound() {
+		cout << name << " Meow meow" << endl;
+	}
+};
+
+
+class CheshirCat : public Cat {
+public:
+	void Sound() override {
+		cout << name << " Smile" << endl;
+	}
+};
+
+void info(Animal* animal) {
+	cout << "This is " << animal->name << endl;
+	animal->Sound();
+}
+
+void info(Animal& animal) {
+	cout << "This is " << animal.name << endl;
+	animal.Sound();
+}
+int main() {
+	
+}
