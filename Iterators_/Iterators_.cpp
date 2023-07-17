e72c7f8e3f395465a8604bd80628f1bc1b48d550
Zamanof <Zamanov@itstep.edu.az>
2023-07-17 17:58:47 +0400

    ST Vector

---
+++ Iterators_/Iterators_.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<iterator>
+using namespace std;
+
+int main() {
+	//int arr[]{ 1, 98, 33, 99, 78 };
+	//for (size_t i = 0; i < 5; i++)
+	//{
+	//	cout << arr[i] << ' ';
+	//}
+	//cout << '\n';
+
+	//int* a = arr;
+	///*cout << sizeof(arr) << endl;
+	//cout << sizeof(a) << endl;*/
+
+	//for (size_t i = 0; i < 5; i++)
+	//{
+	//	cout << a[i] << ' ';
+	//}
+	//
+	//cout << '\n';
+
+	//for (int item : arr) {
+	//	item *= 10;
+	//	cout << item << ' ';
+	//}
+	//cout << '\n';
+
+	//for (int item : arr) {
+	//	cout << item << ' ';
+	//}
+	//cout << '\n';
+
+	/*
+		input			==, !=, *i, i++, ++i, *i++
+		output			++i, i++, *i = value, *i++ = value
+		forward			==, !=, *i, ++i, i++, *i++
+		bidirectional	==, !=, *i, ++i, i++, *i++, --i, i--, *i--
+		random access	i += n, i + n, i -= n, i - n, i[n], i1 - i2,
+						<, >, <=, >=, ==, !=, *i, ++i, i++, *i++, --i, i--, *i--
+	
+		advance(InIter &iter, Dist d)
+		distance(InIter &start, InIter &start)
+	*/
+
+
+}
---
+++ vector container/vector cotainer.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<iterator>
+#include<vector>
+using namespace std;
+
+void show(const vector<int>& vec) {
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		cout << vec.at(i) << ' ';
+	}
+	cout << '\n';
+}
+
+int main() {
+	vector<int> vec;
+	/*cout <<"Vector size\t: " << vec.size() << endl;
+	cout <<"Vector capacity\t: " << vec.capacity() << endl << endl;*/
+	vec.push_back(25);
+	//cout << "Vector size\t: " << vec.size() << endl;
+	//cout << "Vector capacity\t: " << vec.capacity() << endl << endl;
+
+	for (size_t i = 0; i < 40; i++)
+	{
+		vec.push_back(i);
+		/*cout << "Vector size\t: " << vec.size() << endl;
+		cout << "Vector capacity\t: " << vec.capacity() << endl << endl;*/
+	}
+	/*show(vec);
+
+	vec.clear();
+	cout << "Vector size\t: " << vec.size() << endl;
+	cout << "Vector capacity\t: " << vec.capacity() << endl << endl;
+	cout << vec[1] << endl;*/
+
+	/*vec.empty();*/
+	vector < vector<int>> vectorRoot;
+	vector<int> vectorChild{2, 3};
+	vectorRoot.push_back(vectorChild);
+	cout << vectorRoot[0][1] << endl;
+}
